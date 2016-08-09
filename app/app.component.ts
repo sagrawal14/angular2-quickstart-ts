9dd5c40c98f2ac853069e439e559a923256f7eae
Shashank Agrawal <shagrawal99@gmail.com>
2016-08-09 15:18:03 +0530

    Created the Angular2 quick start app.

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by shashank on 25/07/16.
+ */
+
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+
+export class AppComponent { }
